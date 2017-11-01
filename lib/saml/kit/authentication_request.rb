7d61e4d4b08d1d9819ffef847f32bb6b503288a8
mo <mo.khan@gmail.com>
2017-11-01 12:03:03 -0600

    add a digital signature to the authnrequest.

--- lib/saml/kit/authentication_request.rb
+++ lib/saml/kit/authentication_request.rb
@@ -42,6 +42,7 @@ module Saml
         end
 
         def to_xml(xml = ::Builder::XmlMarkup.new)
+          signature = Signature.new(id)
           xml.tag!('samlp:AuthnRequest',
                    "xmlns:samlp" => "urn:oasis:names:tc:SAML:2.0:protocol",
                    "xmlns:saml" => "urn:oasis:names:tc:SAML:2.0:assertion",
@@ -50,10 +51,11 @@ module Saml
                    IssueInstant: issued_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
                    AssertionConsumerServiceURL: acs_url,
                   ) do
+            signature.template(xml)
             xml.tag!('saml:Issuer', issuer)
             xml.tag!('samlp:NameIDPolicy', Format: "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress")
           end
-          xml.target!
+          signature.finalize(xml)
         end
 
         def build
