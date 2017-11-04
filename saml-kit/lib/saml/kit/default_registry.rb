66698b49651a663adeefac776590b20b4abbeeca
mo <mo.khan@gmail.com>
2017-11-04 12:29:47 -0600

    implement simple registry registration.

--- saml-kit/lib/saml/kit/default_registry.rb
+++ saml-kit/lib/saml/kit/default_registry.rb
@@ -1,7 +1,16 @@
 module Saml
   module Kit
     class DefaultRegistry
-      def service_provider_metadata_for(issuer)
+      def initialize(items = {})
+        @items = items
+      end
+
+      def register(metadata)
+        @items[metadata.entity_id] = metadata
+      end
+
+      def service_provider_metadata_for(entity_id)
+        @items[entity_id]
       end
     end
   end
