67bee25bf3f5f0f082c2d10f924625cb9e549a88
mo <mo@mokhan.ca>
2017-11-07 15:03:02 -0700

    sync up entity_ids.

--- airport/config/initializers/saml_kit.rb
+++ airport/config/initializers/saml_kit.rb
@@ -1,5 +1,6 @@
-DEFAULT_IDP_ENTITY_ID="#{ENV['AUTHENTICATION_HOST']}/metadata"
+idp = nil
 Saml::Kit.configure do |configuration|
   configuration.issuer = ENV['ISSUER']
-  configuration.registry.register_url(DEFAULT_IDP_ENTITY_ID)
+  idp = configuration.registry.register_url("#{ENV['AUTHENTICATION_HOST']}/metadata")
 end
+DEFAULT_IDP_ENTITY_ID=idp.entity_id
--- proof/app/controllers/metadata_controller.rb
+++ proof/app/controllers/metadata_controller.rb
@@ -8,7 +8,6 @@ class MetadataController < ApplicationController
   def to_xml
     builder = Saml::Kit::IdentityProviderMetadata::Builder.new
     builder.contact_email = 'hi@example.com'
-    builder.entity_id = metadata_url
     builder.organization_name = "Acme, Inc"
     builder.organization_url = root_url
     builder.add_single_sign_on_service(new_session_url, binding: :post)
