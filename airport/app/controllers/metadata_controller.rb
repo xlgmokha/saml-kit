a8f90fe31ee5f4c79682be3fc9c280c5730c9cc8
mo <mo.khan@gmail.com>
2017-11-10 16:42:53 -0700

    cache metadata.

--- airport/app/controllers/metadata_controller.rb
+++ airport/app/controllers/metadata_controller.rb
@@ -8,9 +8,11 @@ class MetadataController < ApplicationController
   private
 
   def to_xml
-    builder = Saml::Kit::ServiceProviderMetadata::Builder.new
-    builder.sign = false
-    builder.add_assertion_consumer_service(session_url, binding: :post)
-    builder.to_xml
+    Rails.cache.fetch(metadata_url, expires_in: 1.hour) do
+      builder = Saml::Kit::ServiceProviderMetadata::Builder.new
+      builder.sign = false
+      builder.add_assertion_consumer_service(session_url, binding: :post)
+      builder.to_xml
+    end
   end
 end
