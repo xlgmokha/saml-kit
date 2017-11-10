23ecfc5774883f624a3d554af30da1e95ead4318
mo <mo.khan@gmail.com>
2017-11-10 12:19:51 -0700

    add zlib compression support.

--- spec/saml/request_spec.rb
+++ spec/saml/request_spec.rb
@@ -7,7 +7,23 @@ RSpec.describe Saml::Kit::Request do
     it 'returns a compressed and base64 encoded document' do
       xml = "<xml></xml>"
       document = double(to_xml: xml)
-      expect(subject.encode(document)).to eql(Base64.encode64(xml))
+
+      expected_value = Base64.encode64(Zlib::Deflate.deflate(xml, 9)).gsub(/\n/, '')
+      expect(subject.encode(document)).to eql(expected_value)
+    end
+  end
+
+  describe ".decode" do
+    subject { described_class }
+    let(:issuer) { FFaker::Internet.http_url }
+
+    it 'decodes the raw_request' do
+      builder = Saml::Kit::AuthenticationRequest::Builder.new
+      builder.issuer = issuer
+      raw_saml = subject.encode(builder)
+
+      result = subject.decode(raw_saml)
+      expect(result.issuer).to eql(issuer)
     end
   end
 end
