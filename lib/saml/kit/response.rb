02088483c7b8c667de1c8752682fa21d337a2350
mo <mo.khan@gmail.com>
2017-11-10 14:54:39 -0700

    use sign method instead of explict call to finalize.

--- lib/saml/kit/response.rb
+++ lib/saml/kit/response.rb
@@ -217,45 +217,45 @@ module Saml
           true
         end
 
-        def to_xml(xml = ::Builder::XmlMarkup.new)
-          signature = Signature.new(id, sign: want_assertions_signed)
-          xml.Response response_options do
-            xml.Issuer(issuer, xmlns: Namespaces::ASSERTION)
-            signature.template(xml)
-            xml.Status do
-              xml.StatusCode Value: status_code
-            end
-            xml.Assertion(assertion_options) do
-              xml.Issuer issuer
-              xml.Subject do
-                xml.NameID user.name_id_for(request), Format: request.name_id_format
-                xml.SubjectConfirmation Method: Namespaces::BEARER do
-                  xml.SubjectConfirmationData "", subject_confirmation_data_options
-                end
+        def to_xml
+          Signature.sign(id, sign: want_assertions_signed) do |xml, signature|
+            xml.Response response_options do
+              xml.Issuer(issuer, xmlns: Namespaces::ASSERTION)
+              signature.template(xml)
+              xml.Status do
+                xml.StatusCode Value: status_code
               end
-              xml.Conditions conditions_options do
-                xml.AudienceRestriction do
-                  xml.Audience request.issuer
+              xml.Assertion(assertion_options) do
+                xml.Issuer issuer
+                xml.Subject do
+                  xml.NameID user.name_id_for(request), Format: request.name_id_format
+                  xml.SubjectConfirmation Method: Namespaces::BEARER do
+                    xml.SubjectConfirmationData "", subject_confirmation_data_options
+                  end
                 end
-              end
-              xml.AuthnStatement authn_statement_options do
-                xml.AuthnContext do
-                  xml.AuthnContextClassRef Namespaces::PASSWORD
+                xml.Conditions conditions_options do
+                  xml.AudienceRestriction do
+                    xml.Audience request.issuer
+                  end
                 end
-              end
-              assertion_attributes = user.assertion_attributes_for(request)
-              if assertion_attributes.any?
-                xml.AttributeStatement do
-                  assertion_attributes.each do |key, value|
-                    xml.Attribute Name: key, NameFormat: Namespaces::URI, FriendlyName: key do
-                      xml.AttributeValue value.to_s
+                xml.AuthnStatement authn_statement_options do
+                  xml.AuthnContext do
+                    xml.AuthnContextClassRef Namespaces::PASSWORD
+                  end
+                end
+                assertion_attributes = user.assertion_attributes_for(request)
+                if assertion_attributes.any?
+                  xml.AttributeStatement do
+                    assertion_attributes.each do |key, value|
+                      xml.Attribute Name: key, NameFormat: Namespaces::URI, FriendlyName: key do
+                        xml.AttributeValue value.to_s
+                      end
                     end
                   end
                 end
               end
             end
           end
-          signature.finalize(xml)
         end
 
         def build
