51120aab2692e50c81d9c45e385cede10a432403
mo <mo.khan@gmail.com>
2017-11-10 12:46:17 -0700

    return InvalidRequest when saml cannot be deserialized.

--- spec/saml/request_spec.rb
+++ spec/saml/request_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe Saml::Kit::Request do
   describe ".decode" do
     subject { described_class }
     let(:issuer) { FFaker::Internet.http_url }
+    let(:registry) { instance_double(Saml::Kit::DefaultRegistry) }
+    let(:service_provider_metadata) { instance_double(Saml::Kit::ServiceProviderMetadata) }
+
+    before :each do
+      allow(Saml::Kit.configuration).to receive(:registry).and_return(registry)
+      allow(registry).to receive(:metadata_for).and_return(service_provider_metadata)
+      allow(service_provider_metadata).to receive(:matches?).and_return(true)
+      allow(service_provider_metadata).to receive(:assertion_consumer_services).and_return([
+        { location: FFaker::Internet.http_url, binding: Saml::Kit::Namespaces::POST }
+      ])
+    end
 
     it 'decodes the raw_request' do
       builder = Saml::Kit::AuthenticationRequest::Builder.new
@@ -24,6 +35,11 @@ RSpec.describe Saml::Kit::Request do
 
       result = subject.decode(raw_saml)
       expect(result.issuer).to eql(issuer)
+      expect(result).to be_valid
+    end
+
+    it 'returns an invalid request when the raw request is corrupted' do
+      expect(subject.decode("nonsense")).to be_invalid
     end
   end
 end
