a687f6a985c4aa4545cff3759dbd8d7e470d8fab
mo <mo.khan@gmail.com>
2017-11-10 12:31:12 -0700

    inline Request.authentication.

--- lib/saml/kit/request.rb
+++ lib/saml/kit/request.rb
@@ -5,13 +5,6 @@ module Saml
         Saml::Kit::Content.encode_raw_saml(document.to_xml)
       end
 
-      def self.authentication(assertion_consumer_service:, entity_id: nil)
-        builder = AuthenticationRequest::Builder.new
-        builder.acs_url = assertion_consumer_service
-        builder.entity_id = entity_id unless entity_id.blank?
-        encode(builder)
-      end
-
       def self.decode(raw_request)
         request = Saml::Kit::Content.decode_raw_saml(raw_request)
         AuthenticationRequest.new(request)
