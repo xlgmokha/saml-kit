0480ed684dbb9f8db7826db821794f5ea48b7fc6
mo <mo.khan@gmail.com>
2017-11-12 14:58:58 -0700

    add signature to logout request.

--- saml-kit/spec/saml/logout_request_spec.rb
+++ saml-kit/spec/saml/logout_request_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Saml::Kit::LogoutRequest do
       result = subject.to_xml
       xml_hash = Hash.from_xml(result)
 
-      expect(xml_hash['LogoutRequest']['ID']).to eql(subject.id)
+      expect(xml_hash['LogoutRequest']['ID']).to eql("_#{subject.id}")
       expect(xml_hash['LogoutRequest']['Version']).to eql("2.0")
       expect(xml_hash['LogoutRequest']['IssueInstant']).to eql(Time.now.utc.iso8601)
       expect(xml_hash['LogoutRequest']['Destination']).to eql(subject.destination)
@@ -24,6 +24,21 @@ RSpec.describe Saml::Kit::LogoutRequest do
       expect(xml_hash['LogoutRequest']['Issuer']).to eql(subject.issuer)
       expect(xml_hash['LogoutRequest']['NameID']).to eql(name_id)
       expect(result).to have_xpath("//LogoutRequest//NameID[@Format=\"#{subject.name_id_format}\"]")
+    end
+
+    it 'includes a signature by default' do
+      travel_to 1.second.from_now
+      xml_hash = Hash.from_xml(subject.to_xml)
+
+      expect(xml_hash['LogoutRequest']['Signature']).to be_present
+    end
+
+    it 'excludes a signature' do
+      travel_to 1.second.from_now
+      subject.sign = false
+      xml_hash = Hash.from_xml(subject.to_xml)
+
+      expect(xml_hash['LogoutRequest']['Signature']).to be_nil
     end
   end
 end
