9e6196d4d25555c15b4a647dbd8e10ecf8db7aee
mo <mo.khan@gmail.com>
2017-11-12 14:58:58 -0700

    add signature to logout request.

--- lib/saml/kit/logout_request.rb
+++ lib/saml/kit/logout_request.rb
@@ -3,6 +3,7 @@ module Saml
     class LogoutRequest
       class Builder
         attr_accessor :id, :destination, :issuer, :name_id_format, :now
+        attr_accessor :sign
         attr_reader :user
 
         def initialize(user, configuration: Saml::Kit.configuration)
@@ -11,23 +12,25 @@ module Saml
           @issuer = configuration.issuer
           @name_id_format = Saml::Kit::Namespaces::PERSISTENT
           @now = Time.now.utc
+          @sign = true
         end
 
         def to_xml
-          xml = ::Builder::XmlMarkup.new
-          xml.instruct!
-          xml.LogoutRequest logout_request_options do
-            xml.Issuer issuer
-            xml.NameID name_id_options, user.name_id_for(self)
+          Signature.sign(id, sign: sign) do |xml, signature|
+            xml.instruct!
+            xml.LogoutRequest logout_request_options do
+              xml.Issuer issuer
+              signature.template(xml)
+              xml.NameID name_id_options, user.name_id_for(self)
+            end
           end
-          xml.target!
         end
 
         private
 
         def logout_request_options
           {
-            ID: id,
+            ID: "_#{id}",
             Version: "2.0",
             IssueInstant: now.utc.iso8601,
             Destination: destination,
