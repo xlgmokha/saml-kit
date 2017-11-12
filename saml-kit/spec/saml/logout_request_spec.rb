5cd6136c570389ff00dbf0c5d3e263c84a757280
mo <mo.khan@gmail.com>
2017-11-12 14:36:05 -0700

    start to build logout request.

---
+++ saml-kit/spec/saml/logout_request_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Saml::Kit::LogoutRequest do
+  describe described_class::Builder do
+    subject { described_class.new(user) }
+    let(:user) { double(:user, name_id_for: name_id) }
+    let(:name_id) { SecureRandom.uuid }
+
+    it 'produces the expected xml' do
+      travel_to 1.second.from_now
+      subject.id = SecureRandom.uuid
+      subject.destination = FFaker::Internet.http_url
+      subject.issuer = FFaker::Internet.http_url
+      subject.name_id_format = Saml::Kit::Namespaces::TRANSIENT
+
+      result = subject.to_xml
+      xml_hash = Hash.from_xml(result)
+
+      expect(xml_hash['LogoutRequest']['ID']).to eql(subject.id)
+      expect(xml_hash['LogoutRequest']['Version']).to eql("2.0")
+      expect(xml_hash['LogoutRequest']['IssueInstant']).to eql(Time.now.utc.iso8601)
+      expect(xml_hash['LogoutRequest']['Destination']).to eql(subject.destination)
+
+      expect(xml_hash['LogoutRequest']['Issuer']).to eql(subject.issuer)
+      expect(xml_hash['LogoutRequest']['NameID']).to eql(name_id)
+      doc = Nokogiri::XML(result)
+      expect(doc.xpath("//LogoutRequest//NameID[@Format=\"#{subject.name_id_format}\"]")).to be_present
+    end
+  end
+end
