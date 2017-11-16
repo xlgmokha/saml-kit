c6955e8204819a3d973bae03f221887ed1253a2b
mo <mo@mokhan.ca>
2017-11-16 11:45:55 -0700

    generate SAMLResponse key if builder builds a response.

--- saml-kit/spec/saml/binding_spec.rb
+++ saml-kit/spec/saml/binding_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe Saml::Kit::Binding do
       let(:subject) { Saml::Kit::Binding.new(binding: Saml::Kit::Namespaces::HTTP_REDIRECT, location: location) }
 
       it 'encodes the request using the HTTP-Redirect encoding' do
-        url, _ = subject.serialize(Saml::Kit::AuthenticationRequest, relay_state: relay_state)
+        builder = Saml::Kit::AuthenticationRequest::Builder.new
+        url, _ = subject.serialize(builder, relay_state: relay_state)
         expect(url).to start_with(location)
         expect(url).to have_query_param('SAMLRequest')
         expect(url).to have_query_param('SigAlg')
@@ -20,8 +21,9 @@ RSpec.describe Saml::Kit::Binding do
     describe "HTTP-POST Binding" do
       let(:subject) { Saml::Kit::Binding.new(binding: Saml::Kit::Namespaces::POST, location: location) }
 
-      it 'encodes the request using the HTTP-POST encoding' do
-        url, saml_params = subject.serialize(Saml::Kit::AuthenticationRequest, relay_state: relay_state)
+      it 'encodes the request using the HTTP-POST encoding for a AuthenticationRequest' do
+        builder = Saml::Kit::AuthenticationRequest::Builder.new
+        url, saml_params = subject.serialize(builder, relay_state: relay_state)
 
         expect(url).to eql(location)
         expect(saml_params['RelayState']).to eql(relay_state)
@@ -30,6 +32,35 @@ RSpec.describe Saml::Kit::Binding do
         expect(xml['AuthnRequest']).to be_present
         expect(xml['AuthnRequest']['Destination']).to eql(location)
         expect(xml['AuthnRequest']['Signature']).to be_present
+      end
+
+      it 'returns a SAMLRequest for a LogoutRequest' do
+        user = double(:user, name_id_for: SecureRandom.uuid)
+        builder = Saml::Kit::LogoutRequest::Builder.new(user)
+        url, saml_params = subject.serialize(builder, relay_state: relay_state)
+
+        expect(url).to eql(location)
+        expect(saml_params['RelayState']).to eql(relay_state)
+        expect(saml_params['SAMLRequest']).to be_present
+        xml = Hash.from_xml(Base64.decode64(saml_params['SAMLRequest']))
+        expect(xml['LogoutRequest']).to be_present
+        expect(xml['LogoutRequest']['Destination']).to eql(location)
+        expect(xml['LogoutRequest']['Signature']).to be_present
+      end
+
+      it 'returns a SAMLResponse for a LogoutResponse' do
+        user = double(:user, name_id_for: SecureRandom.uuid)
+        request = instance_double(Saml::Kit::AuthenticationRequest, id: SecureRandom.uuid)
+        builder = Saml::Kit::LogoutResponse::Builder.new(user, request)
+        url, saml_params = subject.serialize(builder, relay_state: relay_state)
+
+        expect(url).to eql(location)
+        expect(saml_params['RelayState']).to eql(relay_state)
+        expect(saml_params['SAMLResponse']).to be_present
+        xml = Hash.from_xml(Base64.decode64(saml_params['SAMLResponse']))
+        expect(xml['LogoutResponse']).to be_present
+        expect(xml['LogoutResponse']['Destination']).to eql(location)
+        expect(xml['LogoutResponse']['Signature']).to be_present
       end
     end
 
