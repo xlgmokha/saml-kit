89c12eec9b05054f0d3b5790ae7b16512de7c350
mo <mo@mokhan.ca>
2017-11-16 11:56:25 -0700

    exclude the RelayState when absent.

--- saml-kit/lib/saml/kit/binding.rb
+++ saml-kit/lib/saml/kit/binding.rb
@@ -24,8 +24,8 @@ module Saml
           document = builder.build
           saml_params = {
             document.query_string_parameter => Base64.strict_encode64(document.to_xml),
-            'RelayState' => relay_state,
           }
+          saml_params['RelayState'] = relay_state if relay_state.present?
           [location, saml_params]
         else
           []
