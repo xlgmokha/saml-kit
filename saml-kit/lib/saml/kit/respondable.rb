fba8e4e8141b9f45a8774b45985a32cfe9aa23b2
mo <mo@mokhan.ca>
2017-11-17 19:50:21 -0700

    push up request id match validation.

--- saml-kit/lib/saml/kit/respondable.rb
+++ saml-kit/lib/saml/kit/respondable.rb
@@ -2,9 +2,11 @@ module Saml
   module Kit
     module Respondable
       extend ActiveSupport::Concern
+      attr_reader :request_id
 
       included do
         validates_inclusion_of :status_code, in: [Namespaces::SUCCESS]
+        validate :must_match_request_id
       end
 
       def query_string_parameter
@@ -17,6 +19,16 @@ module Saml
 
       def in_response_to
         to_h.fetch(name, {}).fetch('InResponseTo', nil)
+      end
+
+      private
+
+      def must_match_request_id
+        return if request_id.nil?
+
+        if in_response_to != request_id
+          errors[:in_response_to] << error_message(:invalid_response_to)
+        end
       end
     end
   end
