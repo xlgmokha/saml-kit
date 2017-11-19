c67d5193c96be28a3dc69f7c3cf354a5d7e2c815
mo <mo.khan@gmail.com>
2017-11-19 13:16:23 -0700

    extract query_for helper.

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -1,9 +1,13 @@
 module TestHelpers
   def query_params_from(url)
-    Hash[uri_for(url).query.split("&").map { |x| x.split('=', 2) }]
+    Hash[query_for(url).split("&").map { |x| x.split('=', 2) }]
   end
 
   def uri_for(url)
     URI.parse(url)
+  end
+
+  def query_for(url)
+    uri_for(url).query
   end
 end
