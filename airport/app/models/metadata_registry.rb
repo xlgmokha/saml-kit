467818298e58356c4e983bc5fc775984d707c4ab
mo <mo@mokhan.ca>
2017-11-23 15:34:41 -0700

    fix registry.

--- airport/app/models/metadata_registry.rb
+++ airport/app/models/metadata_registry.rb
@@ -5,14 +5,14 @@ class MetadataRegistry
   end
 
   def register(metadata)
-    record = Metadatum.find_or_create_by!(issuer, metadata.entity_id)
+    record = Metadatum.find_or_create_by!(issuer: metadata.entity_id)
     record.metadata = metadata.to_xml
     record.save!
     metadata
   end
 
   def metadata_for(entity_id)
-    Saml::Kit::Metadata.from(Metadatum.find_by!(entity_id: entity_id).metadata)
+    Metadatum.find_by!(issuer: entity_id).to_saml
   rescue ActiveRecord::RecordNotFound => error
     Rails.logger.error(error)
     nil
--- airport/app/models/metadatum.rb
+++ airport/app/models/metadatum.rb
@@ -1,2 +1,5 @@
 class Metadatum < ApplicationRecord
+  def to_saml
+    Saml::Kit::Metadata.from(metadata)
+  end
 end
