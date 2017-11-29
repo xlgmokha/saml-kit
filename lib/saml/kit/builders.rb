6ab045939ed9b814742f5ee8385ed1af87635e66
mo <mo@mokhan.ca>
2017-11-28 19:38:30 -0700

    move idp builder to separate file.

--- lib/saml/kit/builders.rb
+++ lib/saml/kit/builders.rb
@@ -1,1 +1,2 @@
 require 'saml/kit/builders/authentication_request'
+require 'saml/kit/builders/identity_provider_metadata'
---
+++ spec/saml/builders/identity_provider_metadata_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+RSpec.describe Saml::Kit::IdentityProviderMetadata::Builder do
+  subject { described_class.new }
+  let(:email) { FFaker::Internet.email }
+  let(:org_name) { FFaker::Movie.title }
+  let(:url) { "https://#{FFaker::Internet.domain_name}" }
+  let(:entity_id) { FFaker::Movie.title }
+
+  it 'builds a proper metadata' do
+    subject.contact_email = email
+    subject.entity_id = entity_id
+    subject.organization_name = org_name
+    subject.organization_url = url
+    subject.name_id_formats = [
+      Saml::Kit::Namespaces::PERSISTENT,
+      Saml::Kit::Namespaces::TRANSIENT,
+      Saml::Kit::Namespaces::EMAIL_ADDRESS,
+    ]
+    subject.add_single_sign_on_service("https://www.example.com/login", binding: :http_redirect)
+    subject.add_single_logout_service("https://www.example.com/logout", binding: :http_post)
+    subject.attributes << "id"
+
+    result = Hash.from_xml(subject.build.to_xml)
+
+    expect(result['EntityDescriptor']['ID']).to be_present
+    expect(result['EntityDescriptor']['entityID']).to eql(entity_id)
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['protocolSupportEnumeration']).to eql(Saml::Kit::Namespaces::PROTOCOL)
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['WantAuthnRequestsSigned']).to eql('true')
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['NameIDFormat']).to match_array([
+      Saml::Kit::Namespaces::PERSISTENT,
+      Saml::Kit::Namespaces::TRANSIENT,
+      Saml::Kit::Namespaces::EMAIL_ADDRESS,
+    ])
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['SingleSignOnService']['Binding']).to eql(Saml::Kit::Bindings::HTTP_REDIRECT)
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['SingleSignOnService']['Location']).to eql("https://www.example.com/login")
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['SingleLogoutService']['Binding']).to eql(Saml::Kit::Bindings::HTTP_POST)
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['SingleLogoutService']['Location']).to eql("https://www.example.com/logout")
+    expect(result['EntityDescriptor']['IDPSSODescriptor']['Attribute']['Name']).to eql("id")
+    certificates = result['EntityDescriptor']['IDPSSODescriptor']['KeyDescriptor'].map { |x| x['KeyInfo']['X509Data']['X509Certificate'] }
+    expect(certificates).to match_array([
+      Saml::Kit.configuration.stripped_signing_certificate,
+      Saml::Kit.configuration.stripped_encryption_certificate,
+    ])
+    expect(result['EntityDescriptor']['Organization']['OrganizationName']).to eql(org_name)
+    expect(result['EntityDescriptor']['Organization']['OrganizationDisplayName']).to eql(org_name)
+    expect(result['EntityDescriptor']['Organization']['OrganizationURL']).to eql(url)
+    expect(result['EntityDescriptor']['ContactPerson']['contactType']).to eql("technical")
+    expect(result['EntityDescriptor']['ContactPerson']['Company']).to eql("mailto:#{email}")
+  end
+end
