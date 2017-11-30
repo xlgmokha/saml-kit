7d815da4eb510a1e65dc611e84865c076e72410c
mo <mo@mokhan.ca>
2017-11-30 10:13:41 -0700

    remove duplicate test.

--- spec/saml/authentication_request_spec.rb
+++ spec/saml/authentication_request_spec.rb
@@ -24,30 +24,6 @@ RSpec.describe Saml::Kit::AuthenticationRequest do
   it { expect(subject.name_id_format).to eql(name_id_format) }
   it { expect(subject.destination).to eql(destination) }
 
-  describe "#to_xml" do
-    subject { Saml::Kit::Builders::AuthenticationRequest.new(configuration: configuration) }
-    let(:configuration) do
-      config = Saml::Kit::Configuration.new
-      config.issuer = issuer
-      config
-    end
-    let(:issuer) { FFaker::Movie.title }
-    let(:acs_url) { "https://airport.dev/session/acs" }
-
-    it 'returns a valid authentication request' do
-      travel_to 1.second.from_now
-      subject.acs_url = acs_url
-      result = Hash.from_xml(subject.to_xml)
-
-      expect(result['AuthnRequest']['ID']).to be_present
-      expect(result['AuthnRequest']['Version']).to eql('2.0')
-      expect(result['AuthnRequest']['IssueInstant']).to eql(Time.now.utc.iso8601)
-      expect(result['AuthnRequest']['AssertionConsumerServiceURL']).to eql(acs_url)
-      expect(result['AuthnRequest']['Issuer']).to eql(issuer)
-      expect(result['AuthnRequest']['NameIDPolicy']['Format']).to eql(Saml::Kit::Namespaces::PERSISTENT)
-    end
-  end
-
   describe "#valid?" do
     let(:registry) { instance_double(Saml::Kit::DefaultRegistry) }
     let(:metadata) { instance_double(Saml::Kit::ServiceProviderMetadata) }
