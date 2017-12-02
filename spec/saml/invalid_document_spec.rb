b26e13b5e76d20944e9f64610a49029524425253
mo <mo.khan@gmail.com>
2017-12-01 19:37:50 -0700

    add missing spec.

---
+++ spec/saml/invalid_document_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe Saml::Kit::InvalidDocument do
+  subject { described_class.new(xml) }
+  let(:xml) { "<xml></xml>" }
+
+  it 'is invalid' do
+    expect(subject).to be_invalid
+    expect(subject.errors[:base]).to be_present
+  end
+end
+
