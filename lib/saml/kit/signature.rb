1e5e8622c2ce3da94a79a0869b86fee39bf45b52
mo <mo@mokhan.ca>
2017-12-11 15:11:22 -0700

    remove duplication.

--- lib/saml/kit/signature.rb
+++ lib/saml/kit/signature.rb
@@ -1,71 +1,35 @@
 module Saml
   module Kit
     class Signature
-      SIGNATURE_METHODS = {
-        SHA1: "http://www.w3.org/2000/09/xmldsig#rsa-sha1",
-        SHA224: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha224",
-        SHA256: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
-        SHA384: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha384",
-        SHA512: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha512",
-      }.freeze
-      DIGEST_METHODS = {
-        SHA1: "http://www.w3.org/2000/09/xmldsig#SHA1",
-        SHA224: "http://www.w3.org/2001/04/xmldsig-more#sha224",
-        SHA256: "http://www.w3.org/2001/04/xmlenc#sha256",
-        SHA384: "http://www.w3.org/2001/04/xmldsig-more#sha384",
-        SHA512: "http://www.w3.org/2001/04/xmlenc#sha512",
-      }.freeze
-
       attr_reader :sign, :xml
-      attr_reader :stripped_signing_certificate
-      attr_reader :private_key
       attr_reader :configuration
 
       def initialize(xml, configuration:, sign: true)
         @configuration = configuration
-        @private_key = configuration.signing_private_key
-        @reference_ids = []
         @sign = sign
-        @stripped_signing_certificate = configuration.stripped_signing_certificate
         @xml = xml
-      end
-
-      def signature_method
-        SIGNATURE_METHODS[configuration.signature_method]
-      end
-
-      def digest_method
-        DIGEST_METHODS[configuration.digest_method]
       end
 
       def template(reference_id)
         return unless sign
-        return if reference_id.blank?
-        @reference_ids << reference_id
-        Template.new(self).to_xml(xml: xml)
-      end
-
-      def reference_id
-        @reference_ids.last
+        signature = signatures.build(reference_id)
+        Template.new(signature).to_xml(xml: xml)
       end
 
       def finalize
-        sign ? apply_to(xml.target!) : xml.target!
-      end
-
-      def apply_to(raw_xml)
-        return raw_xml unless sign
-
-        @reference_ids.each do |reference_id|
-          raw_xml = Xmldsig::SignedDocument.new(raw_xml).sign(private_key)
-        end
-        raw_xml
+        signatures.complete(xml.target!)
       end
 
       def self.sign(sign: true, xml: ::Builder::XmlMarkup.new, configuration: Saml::Kit.configuration)
         signature = new(xml, sign: sign, configuration: configuration)
         yield xml, signature
         signature.finalize
+      end
+
+      private
+
+      def signatures
+        @signatures ||= Saml::Kit::Signatures.new(configuration: configuration, sign: sign)
       end
     end
   end
