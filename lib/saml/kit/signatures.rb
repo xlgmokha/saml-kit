19e60d28b9c425214c986319f9e1e550b6485cac
mo <mo@mokhan.ca>
2017-12-11 15:20:43 -0700

    remove unneccessary loop.

--- lib/saml/kit/signatures.rb
+++ lib/saml/kit/signatures.rb
@@ -5,22 +5,23 @@ module Saml
 
       def initialize(configuration:, sign: true)
         @configuration = configuration
-        @reference_ids = []
         @sign = sign
       end
 
       def build(reference_id)
-        @reference_ids << reference_id
         XmlSignature.new(reference_id, configuration: configuration, sign: sign)
       end
 
       def complete(raw_xml)
         return raw_xml unless sign
 
-        @reference_ids.each do |reference_id|
-          raw_xml = Xmldsig::SignedDocument.new(raw_xml).sign(configuration.signing_private_key)
-        end
-        raw_xml
+        Xmldsig::SignedDocument.new(raw_xml).sign(private_key)
+      end
+
+      private
+
+      def private_key
+        configuration.signing_private_key
       end
     end
   end
