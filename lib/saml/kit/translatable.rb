d6ae117814e8db5b7eaf67e6a67bab3b9f156a5d
mo <mo.khan@gmail.com>
2017-12-15 13:50:08 -0700

    bump version.

--- lib/saml/kit/translatable.rb
+++ lib/saml/kit/translatable.rb
@@ -3,7 +3,6 @@ module Saml
     module Translatable
       def error_message(attribute, type: :invalid)
         I18n.translate(attribute, scope: "saml/kit.errors.#{name}")
-        #errors.generate_message(attribute, type: :invalid)
       end
     end
   end
--- lib/saml/kit/version.rb
+++ lib/saml/kit/version.rb
@@ -1,5 +1,5 @@
 module Saml
   module Kit
-    VERSION = "0.2.4"
+    VERSION = "0.2.5"
   end
 end
