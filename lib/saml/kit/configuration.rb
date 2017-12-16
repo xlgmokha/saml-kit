9f44e307de8a79565a21f3bcc77e5b7b6c31d1c8
mo <mo@mokhan.ca>
2017-12-16 09:44:56 -0700

    extract keypair class.

--- lib/saml/kit/configuration.rb
+++ lib/saml/kit/configuration.rb
@@ -56,19 +56,5 @@ module Saml
         certificates(use: :signing).any?
       end
     end
-
-    class KeyPair
-      attr_reader :certificate, :private_key
-
-      def initialize(certificate, private_key, password, use)
-        @use = use
-        @certificate = Saml::Kit::Certificate.new(certificate, use: use)
-        @private_key = OpenSSL::PKey::RSA.new(private_key, password)
-      end
-
-      def for?(use)
-        @use == use
-      end
-    end
   end
 end
