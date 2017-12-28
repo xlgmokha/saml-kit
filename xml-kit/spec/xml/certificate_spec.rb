b8b523d95bc5a912c4ed9c8d5dcd084de7883a23
mo <mo.khan@gmail.com>
2017-12-28 11:57:14 -0700

    shorten let.

--- xml-kit/spec/xml/certificate_spec.rb
+++ xml-kit/spec/xml/certificate_spec.rb
@@ -1,9 +1,6 @@
 RSpec.describe Xml::Kit::Certificate do
   subject { described_class.new(certificate, use: :signing) }
-  let(:certificate) do
-    cert, _ = generate_key_pair('password')
-    cert
-  end
+  let(:certificate) { generate_key_pair('password')[0] }
 
   describe "#fingerprint" do
     it 'returns a fingerprint' do
