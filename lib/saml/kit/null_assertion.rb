de1a61d40ba764b8b525aae040fb063b2e832918
mo <mo@mokhan.ca>
2018-02-17 14:30:21 -0700

    override methods on null assertion.

--- lib/saml/kit/null_assertion.rb
+++ lib/saml/kit/null_assertion.rb
@@ -5,6 +5,19 @@ module Saml
       include Translatable
       validate :invalid
 
+      def issuer; end
+      def name_id; end
+      def signed?; false end
+      def signature; end
+      def attributes; [] end
+      def started_at; Time.at(0).to_datetime end
+      def expired_at; Time.at(0).to_datetime end
+      def audiences; [] end
+      def encrypted?; false end
+      def decryptable?; false end
+      def present?; false end
+      def to_xml(*args); "" end
+
       def invalid
         errors[:assertion].push(error_message(:invalid))
       end
